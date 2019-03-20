3e261fc81d64740c02afb3f5d32b49f5ed876b16
Don Hamilton III <donhamiltoniii@gmail.com>
2019-03-20 10:49:15 -0400

    Fix list generation in 'app.js'

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,30 +2,30 @@ const app = document.querySelector("#app");
 
 app.textContent = "Hello JS!";
 
-fetch('/authors')
-	.then(response => response.json())
-	.then(authors => {
-		let content = `<ul class="authors">`
-		authors.forEach((author) => {
-			let books = ''
-			author.books.forEach(book => {
-				books += `
-					<li class="book">
-						<h5 class="book__title">${book.title}</h5>
-						<p class="book__description">${book.description}</p>
-					</li>
-				`
-			})
-			content += `
+fetch("/authors")
+  .then(response => response.json())
+  .then(authors => {
+    let content = `<ul class="authors">`;
+    authors.forEach(author => {
+      content += `
 			 	<li class="author">
 			 		<h3 class="author__name">${author.firstName} ${author.lastName}</h3>
 			 		<ul class="books">
-			 			${books}
+			 			${author.books
+              .map(book => {
+                return `
+								<li class="book">
+									<h5 class="book__title">${book.title}</h5>
+									<p class="book__description">${book.description}</p>
+								</li>
+							`;
+              })
+              .join("")}
 			 		</ul>
 			 	</li>
 		 	`;
-		})
-		content += `</ul>`;
-		app.innerHTML = content;
-	})
-	.catch(err => console.log(err))
+    });
+    content += `</ul>`;
+    app.innerHTML = content;
+  })
+  .catch(err => console.log(err));
