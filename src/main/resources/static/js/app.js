9e711716bfbd85d6ba2c9d24cebde9230df40f4a
Columbus Instructor <instructor@wecancodeit.org>
2019-03-21 11:13:04 -0400

    Make functions to replace script

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,31 +1,43 @@
-const app = document.querySelector("#app");
+getRequest('/authors', authors => {
+  getAppContext().innerHTML = renderAuthors(authors);
+})
 
-app.textContent = "Hello JS!";
+function getAppContext() {
+  return document.querySelector("#app")
+}
 
-fetch("/authors")
-  .then(response => response.json())
-  .then(authors => {
-    let content = `<ul class="authors">`;
-    authors.forEach(author => {
-      content += `
-			 	<li class="author">
-			 		<h3 class="author__name">${author.firstName} ${author.lastName}</h3>
-			 		<ul class="books">
-			 			${author.books
-              .map(book => {
-                return `
-								<li class="book">
-									<h5 class="book__title">${book.title}</h5>
-									<p class="book__description">${book.description}</p>
-								</li>
-							`;
-              })
-              .join("")}
-			 		</ul>
-			 	</li>
-		 	`;
-    });
-    content += `</ul>`;
-    app.innerHTML = content;
-  })
-  .catch(err => console.log(err));
+function getRequest(location, callback) {
+  fetch(location)
+    .then(response => response.json())
+    .then(data => callback(data))
+    .catch(err => console.log(err));
+}
+
+function renderAuthors(authors) {
+  return `
+    <ul class="authors">
+      ${authors.map(author => {
+        return `
+          <li class="author">
+            <h3 class="author__name">${author.firstName} ${author.lastName}</h3>
+            <ul class="books">
+              ${renderBooks(author.books)}
+            </ul>
+          </li>
+        `;
+      }).join('')}
+    </ul>
+  `;
+}
+
+function renderBooks(books) {
+  return books
+    .map(book => {
+      return `
+        <li class="book">
+          <h5 class="book__title">${book.title}</h5>
+          <p class="book__description">${book.description}</p>
+        </li>
+      `;
+    }).join('')
+}
